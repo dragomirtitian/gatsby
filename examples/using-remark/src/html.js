df7e22821b352b94202122c60343580873c1d34b
kale-io <cos+a@iuvo.ai>
2018-02-21 13:30:17 +0200

    Enforce explicit boolean attributes notation in JSX Convert JSX attributes without explicit values (e.g., `<App att>`) to their explicit version (e.g., `<App att={true} />`)

--- examples/using-remark/src/html.js
+++ examples/using-remark/src/html.js
@@ -37,7 +37,7 @@ class HTML extends React.Component {
             rel="preload"
             href={`/static/spectral-latin-800.53eca5bf.woff2`}
             as="font"
-            crossOrigin
+            crossOrigin={true}
           />
           <link
             rel="prefetch"
